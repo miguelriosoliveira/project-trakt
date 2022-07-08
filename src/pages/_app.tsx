65a2475906a052213d0f1adb69a80cbe122d1ac1
Miguel Rios <miguelriosoliveira@gmail.com>
2022-07-08 11:21:36 +0200

    lint: Setup ESLint locally

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
+
 import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />;
 }
