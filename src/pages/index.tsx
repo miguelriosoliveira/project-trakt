8bfb08f7c5680a003d733fea029f0ff023de4b54
Miguel Rios <miguelriosoliveira@gmail.com>
2022-07-08 16:38:07 +0200

    ci: Trigger CI 2022-07-08 16:38:07 CEST

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
-
+import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
