9ae10b575a7b654ea3c2e9e904943dcd618e5d1e
Miguel Rios <miguelriosoliveira@gmail.com>
2022-08-02 01:52:46 +0200

    wip: Working on blurred bg on user info section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { FiBarChart2, FiChevronRight } from 'react-icons/fi';
 
 import { Header } from '../components';
 import styles from '../styles/Home.module.scss';
@@ -13,6 +14,35 @@ const Home: NextPage = () => {
 
 			<div className={styles.container}>
 				<Header />
+
+				<main>
+					<section className={styles.userInfoWrapper}>
+						<div className={styles.posterBgWrapper}>
+							<div className={styles.posterBg} />
+						</div>
+
+						<div className={styles.userInfo}>
+							<h1>Hello, Miguel</h1>
+
+							<div>
+								<span>MEMBER SINCE</span>
+								<span>29 JAN 2013 22:17</span>
+							</div>
+
+							<div className={styles.yearToDate}>
+								<FiBarChart2 size={32} strokeWidth={3} />
+								<span className={styles.year}>2022</span>
+								<div className={styles.text}>
+									<p>YEAR</p>
+									<p>TO DATE</p>
+								</div>
+								<FiChevronRight size={16} strokeWidth={3} />
+							</div>
+						</div>
+
+						<div className={styles.stats} />
+					</section>
+				</main>
 			</div>
 		</>
 	);
