eebd473fbfe7198d5d3519ee3e26332cdd1881ee
Miguel Rios <miguelriosoliveira@gmail.com>
2022-11-16 20:46:32 +0100

    fix: Alt to images

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,19 +9,23 @@ export function Header() {
 		<header className={styles.container}>
 			<FiSearch color="white" size={20} strokeWidth={3} />
 
-			<Link href="/">
-				<a className={styles.logo}>
-					<Image
-						src="https://trakt.tv/assets/logos/header@2x-d6926a2c93734bee72c5813819668ad494dbbda651457cd17d15d267bc75c657.png"
-						width={40}
-						height={40}
-					/>
-				</a>
+			<Link href="/" className={styles.logo}>
+				<Image
+					alt="Trakt logo: a red circle with an inclined letter T"
+					src="https://trakt.tv/assets/logos/header@2x-d6926a2c93734bee72c5813819668ad494dbbda651457cd17d15d267bc75c657.png"
+					width={40}
+					height={40}
+				/>
 			</Link>
 
 			<div className={styles.menus}>
 				<FiMenu color="white" size={25} strokeWidth={3} />
-				<Image src="https://github.com/miguelriosoliveira.png" width={35} height={35} />
+				<Image
+					alt="The user's profile picture"
+					src="https://github.com/miguelriosoliveira.png"
+					width={35}
+					height={35}
+				/>
 			</div>
 		</header>
 	);
